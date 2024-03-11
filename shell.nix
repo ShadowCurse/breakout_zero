4c21de19ca4e4e8ef54f540a1e6b2fae39106fd4
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-03-11 22:37:36 +0000

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  shellHook = ''export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
+    pkgs.vulkan-loader
+    pkgs.wayland
+    # needed for now for smithay-client-toolkit dep
+    pkgs.libxkbcommon
+  ]}"'';
+
+  buildInputs = with pkgs; [
+    pkg-config
+    wayland
+    mold
+  ];
+}
